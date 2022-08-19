76c2fc61891230ae7560d9b3b2807875705c9f9e
webdmamune <webd.mamun@gmail.com>
2022-08-19 12:46:39 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        healthdoctheme: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+    ],
+  },
+
+  plugins: [require("daisyui")],
+};
